acb4da33ed4e4c7a9bd757e38f473732d6bb2add
Daniel Montecinos <daniel.montecinos.pinto@gmail.com>
2026-05-23 15:20:36 -0400

    feat(config): add TestConfig with base URL, credentials and timeout constants (#1)

---
+++ SeleniumPractice.Framework/Config/TestConfig.cs
@@ -0,0 +1,13 @@
+namespace SeleniumPractice.Framework.Config;
+
+public static class TestConfig
+{
+  public static string BaseUrl => "https://www.saucedemo.com";
+
+  public static string ValidaUsername => "standard_user";
+  public static string ValidPassword => "secret_sauce";
+  public static string LockedUsername => "locked_out_user";
+
+  public static int DefaultTimeoutSeconds => 10;
+  public static int ShortTimeoutSeconds => 3;
+}
